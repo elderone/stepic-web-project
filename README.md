6dd97a8cce8d14dd910960626df60c242d0acb93
Dmitry Veselov <elderone@ya.ru>
2016-11-24 06:45:29 +0300

    1.8.12 readme & nginx.conf again

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 https://stepik.org/course/Web-технологии-154
 
 for start:
-
+```
 git clone https://github.com/elderone/stepic-web-project.git ~/web
 bash web/init.sh
+```
--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo ﻿ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
