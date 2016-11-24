cf5d29b7e24fba17a10e69c7557374f358e56d04
Dmitry Veselov <elderone@ya.ru>
2016-11-24 08:12:06 +0300

    2.1.11 init.sh add sudo

--- init.sh
+++ init.sh
@@ -4,9 +4,9 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 
-pip3 install django==1.10.3 gunicorn==19.6.0
-sed -i -e "s@/usr/bin/python@/usr/bin/python3@g" /usr/sbin/gunicorn-debian /usr/bin/gunicorn /usr/bin/gunicorn_django /usr/bin/gunicorn_paster
-sed -i -e "s@gunicorn==17.5@gunicorn==19.6.0@g" /usr/bin/gunicorn /usr/bin/gunicorn_django /usr/bin/gunicorn_paster
+sudo pip3 install django==1.10.3 gunicorn==19.6.0
+sudo sed -i -e "s@/usr/bin/python@/usr/bin/python3@g" /usr/sbin/gunicorn-debian /usr/bin/gunicorn /usr/bin/gunicorn_django /usr/bin/gunicorn_paster
+sudo sed -i -e "s@gunicorn==17.5@gunicorn==19.6.0@g" /usr/bin/gunicorn /usr/bin/gunicorn_django /usr/bin/gunicorn_paster
 sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
 sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
 sudo /etc/init.d/gunicorn restart
